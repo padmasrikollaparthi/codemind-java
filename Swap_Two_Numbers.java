1b051dc3e8bf2639dbe326ba04d95bb7ddbe2e73
padmasrikollaparthi <150335735+padmasrikollaparthi@users.noreply.github.com>
2024-07-31 15:52:25 +0530

    import java.util.*; public class Nums{     public static void main(String[] args){         Scanner read = new Scanner(System.in);         int a = read.nextInt();         int b = read.nextInt();         int c = b;         int d = a;         System.out.println(c);         System.out.println(d);     }
    }

---
+++ Swap_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Nums{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c = b;
+        int d = a;
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}

2396a09ac35c9371cc6e4297e8c4b9e3b3b4101c
padmasrikollaparthi <150335735+padmasrikollaparthi@users.noreply.github.com>
2024-08-02 10:06:47 +0530

    import java.util.*; public class Nums{     public static void main(String[] args){         Scanner read = new Scanner(System.in);         int a = read.nextInt();          int sur = 6*a*a;          int v = a*a*a;          System.out.print("Surface area = "+sur+" and ");          System.out.println("Volume = "+v);     } }

---
+++ Surface_Area_and_Volume_of_a_Cube.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Nums{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+         int sur = 6*a*a;
+         int v = a*a*a;
+         System.out.print("Surface area = "+sur+" and ");
+         System.out.println("Volume = "+v);
+    }
+}
